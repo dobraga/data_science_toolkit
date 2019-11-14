911c8bb78765d205e926767706dc3b55206088be
Douglas Braga <douglasmartinsbraga@gmail.com>
2019-11-14 09:10:33 -0300

    Remove old folders

--- preprocess/__init__.py
+++
@@ -1,4 +0,0 @@
-from sklearn.pipeline import make_pipeline, Pipeline
-from sklearn.preprocessing import RobustScaler, PowerTransformer, QuantileTransformer
-from .Ordinal import OrdinalEncoder
-from .Transform import TransformBinary, TransformColumn, TransformImputer, TransformNewColumn, TransformOthers, TransformBoxCox, TransformScaler
