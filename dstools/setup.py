d97b773dd14888b1b5ee5965131b6cadbc07a33e
Douglas Braga <douglasmartinsbraga@gmail.com>
2020-08-13 20:07:09 -0300

    Melhoria na organização dos diretórios

--- dstools/setup.py
+++ dstools/setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
-#with open("README.md", "r") as fh:
-    #long_description = fh.read()
+
+# with open("README.md", "r") as fh:
+# long_description = fh.read()
 setup(
-     name='dstools',  
-     version='0.1',
-     packages=['dstools'],
-     author="Douglas Braga",
-     author_email="douglasmartinsbraga@gmail.com",
-     description="This package is a utils functions to data science.",
-     #long_description=long_description,
-     #long_description_content_type="text/markdown",
-     #url="https://github.com/javatechy/dokr",
-     #packages=setuptools.find_packages(),
-     zip_safe=False
- )
+    name="dstools",
+    version="0.1",
+    packages=["dstools"],
+    author="Douglas Braga",
+    author_email="douglasmartinsbraga@gmail.com",
+    description="This package is a utils functions to data science.",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/javatechy/dokr",
+    # packages=setuptools.find_packages(),
+    zip_safe=False,
+)
